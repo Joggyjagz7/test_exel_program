5b3a7116f94ce9a2cdd894e3f5c14325740accc7
Ejiro Onose <ejiro10000@gmail.com>
2023-09-26 14:57:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
     input_file = st.file_uploader("Upload Source Excel File", type=["xlsx", "xls"])
 
     if input_file is not None:
-        target_file = "https://docs.google.com/spreadsheets/d/1zHHAie1LS_livTnhGKEEJpsCl5X8tVvB0-0g106jixU/edit#gid=0"  # Replace with your target Excel file path
+        target_file = "https://github.com/Joggyjagz7/test_exel_program/blob/main/Main%20sheet.xlsx"  # Replace with your target Excel file path
 
         # Sheet name input
         sheet_name = st.text_input("Enter Sheet Name", "NewSheet")
